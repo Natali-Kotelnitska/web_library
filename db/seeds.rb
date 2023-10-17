4b05ceaa1c32fa69a876da5d2e498b287c1fe930
Natali Kotelnytska <nkotelnicka@gmail.com>
2023-10-17 10:13:38 +0300

    Fix: add FactoryBot to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,11 +5,5 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-20.times do
-  Book.create(
-    title: Faker::Book.title,
-    author: Faker::Book.author,
-    isbn: Faker::Code.isbn,
-    description: Faker::Lorem.paragraph
-  )
-end
+
+FactoryBot.create_list(:book, 20)
