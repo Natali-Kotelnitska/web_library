6233381ad8d58b08be682e18c7c786a86dd11b12
Natali Kotelnytska <nkotelnicka@gmail.com>
2023-11-06 23:58:38 +0200

    Feat(validation): add validation for User and minor configes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, :first_name, :last_name, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 4, maximum: 20 },
+                       format: { with: /\A[a-zA-Z0-9_]+\z/,
+                       message: "can only contain letters, numbers, and underscores" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "books#index"
 
   resources :books
+
+  devise_for :users, skip: :all
 end
