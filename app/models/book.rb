34a8b9f59dcec806e349668611949f2cbbacc565
Natali Kotelnytska <nkotelnicka@gmail.com>
2023-11-22 20:12:49 +0200

    view: refactor view, add attachments

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,6 @@ class Book < ApplicationRecord
   validates :title, :author, presence: true, length: { minimum: 1, maximum: 60 }
   validates :description, length: { maximum: 500 }
   validates :isbn, presence: true, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/ }
-  validates :cover, content_type: [:png, :jpg, :jpeg]
-  validates :content, content_type: [:pdf]
 
   scope :ordered, -> { order(:title) }
 end
