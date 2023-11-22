b1e05ea956ce56c20e5fe990c1d2012b633b68f9
Natali Kotelnytska <nkotelnicka@gmail.com>
2023-11-22 15:42:24 +0200

    feature: add attachment to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,12 @@
 class Book < ApplicationRecord
+  has_one_attached :cover, dependent: :destroy
+  has_one_attached :content, dependent: :destroy
+
   validates :title, :author, presence: true, length: { minimum: 1, maximum: 60 }
   validates :description, length: { maximum: 500 }
   validates :isbn, presence: true, uniqueness: true, format: { with: /\A\d[\d-]*\d\z/ }
+  validates :cover, content_type: [:png, :jpg, :jpeg]
+  validates :content, content_type: [:pdf]
+
   scope :ordered, -> { order(:title) }
 end
