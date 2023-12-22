44e52c95773fcdce8f90e6018392b2f8c02a7414
Natali Kotelnytska <nkotelnicka@gmail.com>
2023-12-22 11:35:19 +0200

    Feature: add Adobe pdf viewer

---
+++ app/javascript/controllers/pdf_viewer_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+
+  connect() {
+    this.setupAdobeDCView();
+  }
+
+  setupAdobeDCView() {
+    const adobeDCView = new AdobeDC.View({
+      clientId: "c7a2dd64ed4740e7b478919fd6b8de8c",
+      divId: "adobe-dc-view",
+    });
+
+    const pdfUrl = this.element.dataset.pdfUrl;
+    const pdfFilename = this.element.dataset.pdfFilename;
+
+    adobeDCView.previewFile(
+      {
+        content: { location: { url: pdfUrl } },
+        metaData: { fileName: pdfFilename },
+      },
+      {}
+    );
+  }
+}
